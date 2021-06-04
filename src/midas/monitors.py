698f3529ec0b951b62a015f7be868c0d204ba02e
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-06-04 21:30:11 +0300

    Setup project settings

---
+++ src/midas/monitors.py
@@ -0,0 +1,33 @@
+from spidermon import MonitorSuite, Monitor
+from spidermon.contrib.actions.telegram.notifiers import SendTelegramMessageSpiderFinished
+from spidermon.contrib.monitors.mixins import StatsMonitorMixin
+from spidermon.contrib.scrapy.monitors import ErrorCountMonitor, UnwantedHTTPCodesMonitor
+from spidermon.decorators import monitors
+
+
+@monitors.name('Item validation')
+class ItemValidationMonitor(Monitor, StatsMonitorMixin):
+
+    @monitors.name('No item validation errors')
+    def test_no_item_validation_errors(self):
+        validation_errors = getattr(
+            self.stats, 'spidermon/validation/fields/errors', 0
+        )
+        self.assertEqual(
+            validation_errors,
+            0,
+            msg='Found validation errors in {} fields'.format(
+                validation_errors)
+        )
+
+
+class SpiderCloseMonitorSuite(MonitorSuite):
+    monitors = [
+        UnwantedHTTPCodesMonitor,
+        ItemValidationMonitor,
+        ErrorCountMonitor,
+    ]
+
+    monitors_finished_actions = [
+        SendTelegramMessageSpiderFinished,
+    ]
--- src/midas/settings.py
+++ src/midas/settings.py
@@ -1,11 +1,4 @@
-# Scrapy settings for midas project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from decouple import config
 
 BOT_NAME = 'midas'
 
@@ -17,30 +10,42 @@ NEWSPIDER_MODULE = 'midas.spiders'
 #USER_AGENT = 'midas (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
+
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
+
+# SpiderMon settings
+SPIDERMON_ENABLED = True
+SPIDERMON_VALIDATION_DROP_ITEMS_WITH_ERRORS = True
+SPIDERMON_TELEGRAM_SENDER_TOKEN = config('TELEGRAM_SENDER_TOKEN')
+SPIDERMON_TELEGRAM_RECIPIENTS = [config('TELEGRAM_RECIPIENTS')]
+SPIDERMON_TELEGRAM_NOTIFIER_INCLUDE_OK_MESSAGES = True
+SPIDERMON_SPIDER_CLOSE_MONITORS = (
+    'midas.monitors.SpiderCloseMonitorSuite',
+)
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -56,15 +61,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+EXTENSIONS = {
+   'spidermon.contrib.scrapy.extensions.Spidermon': 500,
+}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'midas.pipelines.MidasPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'spidermon.contrib.scrapy.pipelines.ItemValidationPipeline': 800,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
