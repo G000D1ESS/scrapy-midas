6318cf46c2494a8c805d4b1010f801808f0d93cf
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-06-05 17:47:49 +0300

    Upgrade HeadHunter spider

--- src/midas/spiders/headhunter.py
+++ src/midas/spiders/headhunter.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import re
 from urllib.parse import urlencode
 
+import dateparser
 from scrapy import Spider, Request
 from w3lib.html import remove_tags, replace_entities
 from w3lib.url import url_query_cleaner
@@ -16,6 +17,7 @@ SEARCH_QUERIES = ['Scrapy', 'Python', 'Django', 'Scraping', 'Brandquad', 'Scrapy
 
 # Regex
 SALARY_REGEX = re.compile(r'(от(?P<minimum>\d+))?(до(?P<maximum>\d+))?', re.IGNORECASE)
+CREATED_AT_REGEX = re.compile(r'\d{1,2}\s+\w+\s+\d{4}')
 
 
 class HeadHunterSpider(Spider):
@@ -23,10 +25,12 @@ class HeadHunterSpider(Spider):
     user_agent = CHROME_USER_AGENT
     custom_settings = {
         'DOWNLOAD_TIMEOUT': 60,
-        'MONGODB_UNIQUE_KEY': 'vacancy_id',
-        'SPIDERMON_VALIDATION_MODELS': (
-            'midas.validators.HeadHunterItem',
-        ),
+        'MONGODB_UNIQUE_KEY': 'offer_id',
+        'PYDANTIC_MODEL': 'midas.serializers.Offer',
+        'ITEM_PIPELINES': {
+            'midas.pipelines.DuplicatesOfferPipeline': 300,
+            'midas.pipelines.PydanticPipeline': 325,
+        },
     }
 
     def start_requests(self):
@@ -45,14 +49,23 @@ class HeadHunterSpider(Spider):
     def parse_vacancy(self, response):
         vacancy_id = response.xpath('//input[@name="vacancyId"]/@value').get()
         vacancy_id = vacancy_id or url_query_cleaner(response.url).rstrip('/').rpartition('/')[-1]
+
+        tags = response.xpath('//div[@class="bloko-tag-list"]//span/text()').getall()
+        tags = [tag.strip().lower() for tag in tags if tag.strip()]
+
+        created_at = remove_tags(response.xpath('//p[has-class("vacancy-creation-time")]').get(''))
+        created_at = CREATED_AT_REGEX.search(created_at.replace('\xa0', ' '))
+        created_at = dateparser.parse(created_at.group()) if created_at else None
+
         yield {
-            'timestamp': int(datetime.now().timestamp()),
-            'vacancy_id': int(vacancy_id),
+            'offer_id': int(vacancy_id),
             'title': remove_tags(response.xpath('//h1[@data-qa="vacancy-title"]').get('')),
             'url': url_query_cleaner(response.url),
             'company': self.get_company_info(response),
+            'city': response.xpath('//p[@data-qa="vacancy-view-location"]/text()').get(''),
             'salary': self.get_salary(response),
-            # 'meta': self.get_meta(response),
+            'created_at': created_at,
+            'tags': tags,
         }
 
     @staticmethod
@@ -69,22 +82,18 @@ class HeadHunterSpider(Spider):
 
     @staticmethod
     def get_salary(response):
-        salary_str = remove_tags(response.xpath('//p[has-class("vacancy-salary")]').get(''))
-        salary_str = salary_str.replace('\xa0', '')
-        salary_range = salary_str.replace(' ', '').lower()
-        salary_range = SALARY_REGEX.search(salary_range).groupdict(default='0')
-        return {'value': salary_str, 'range': salary_range}
-
-    @staticmethod
-    def get_meta(response):
-        pass
+        salary = remove_tags(response.xpath('//p[has-class("vacancy-salary")]').get(''))
+        salary = salary.replace('\xa0', '').replace(' ', '').lower()
+        salary = SALARY_REGEX.search(salary).groupdict(default='0')
+        salary['maximum'] = float(salary['maximum'])
+        salary['minimum'] = float(salary['minimum'])
+        salary['value'] = max(salary['minimum'], salary['maximum'])
+        return salary
 
     @staticmethod
     def get_company_info(response):
         company = response.xpath('//a[has-class("vacancy-company-name")]')
-        if company:
-            return {
-                'name': remove_tags(company.xpath('./*').get()),
-                'url': response.urljoin(company.xpath('./@href').get())
-            }
-        return {'name': '', 'url': ''}
+        return {
+            'name': remove_tags(company.xpath('./*').get()),
+            'url': response.urljoin(company.xpath('./@href').get())
+        }
