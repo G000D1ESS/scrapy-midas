44185c540049fcf48357f94b1e79b37e91b30917
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-06-12 18:37:13 +0300

    Create SearchQueriesMongoDB extension for loading queries from DB

---
+++ src/midas/extensions.py
@@ -0,0 +1,37 @@
+import logging
+
+from pymongo import MongoClient
+from scrapy import signals
+from scrapy.exceptions import NotConfigured
+
+
+class SearchQueriesMongoDB:
+
+    def __init__(self, mongodb_uri, database, collection):
+        self.database = database
+        self.collection = collection
+        self.mongodb_uri = mongodb_uri
+        self.logger = logging.getLogger(__name__)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+
+        mongodb_uri = crawler.settings.get('SEARCH_QUERIES_MONGODB_URI') or crawler.settings.get('MONGODB_URI')
+        database = crawler.settings.get('SEARCH_QUERIES_DATABASE') or crawler.settings.get('MONGODB_DATABASE')
+        collection = crawler.settings.get('SEARCH_QUERIES_COLLECTION')
+
+        if not all([mongodb_uri, database, collection]):
+            raise NotConfigured
+
+        extension = cls(mongodb_uri, database, collection)
+        crawler.signals.connect(extension.spider_opened, signal=signals.spider_opened)
+        return extension
+
+    def spider_opened(self, spider):
+        client = MongoClient(self.mongodb_uri)
+        collection = client[self.database][self.collection]
+        search_queries = collection.aggregate([{'$project': {'_id': 0}}, {'$sort': {'query': 1}}])
+        search_queries = list(search_queries)
+        spider.search_queries = search_queries
+        self.logger.info(f'Loaded {len(search_queries)} search queries')
+        client.close()
